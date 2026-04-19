d6b06f22ae4419c0362bc61a79aed2027ec5dd7b
abhiramyakkala10-ay <abhiramyakkala10@gmail.com>
2026-04-19 16:29:31 +0530

    fix: point vercel to wsgi.py entry point

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
-import sys
+﻿import sys
 import os
 
 path = os.path.abspath(os.path.dirname(__file__))
 if path not in sys.path:
     sys.path.insert(0, path)
 
-from app import app as application
+from app import app
+
+if __name__ == '__main__':
+    app.run()
